b5ab68ccce29804ee1414c8960dc6f481fb636be
Vaishnavi Dhayapule <dhayapulevaishnavi07@gmail.com>
2022-11-25 17:16:09 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -1,51 +1,50 @@
-let nextIndex,curIndex=0,images=[];
-images=[
+let curIndex=0, images = [];
+images = [
     {
-        url: "../images/img1.jfif",
+        url: "../images/img1.jpg",
         alt: "img not found"
     },
     {
-        url: "../images/img2.jfif",
+        url: "../images/img2.jpg",
         alt: "img not found"
     },
     {
-        url: "../images/img3.jfif",
+        url: "../images/img3.jpg",
         alt: "img not found"
     },
     {
-        url: "../images/img4.jfif",
+        url: "../images/img4.jpg",
         alt: "img not found"
     },
     {
-        url: "../images/img5.jfif",
+        url: "../images/img5.jpg",
         alt: "img not found"
     }
 ]
-$(document).ready(function(){
-    addimages();
-    // $(".prev").click(function(e){
-    //     e.preventDefault();
-    // });
+
+list = "";
+dotList="";
+i=0;
+images.forEach((image) => {
+    list += `<div class="slide"><img class="image" src="${image.url}" alt="${image.alt}"> </div>`;
+    dotList+=`<div class="dot" onclick="currentSlide(${i})"></div>`;
+    i++;
 });
+let slideshowContainer = document.getElementById("slideshowContainer");
+slideshowContainer.innerHTML = list;
+let dots=document.getElementById("dots");
+dots.innerHTML=dotList;
+let carouselContainer=document.getElementById("carouselContainer");
+setInterval(function(){
+    curIndex=(curIndex+1)%images.length;
+    let slideshowContainer = document.getElementById("slideshowContainer");
+    slideshowContainer.style.transform=`translateX(-${carouselContainer.clientWidth*curIndex}px)`
+    slideshowContainer.style.transition=`transform 0.5s`;
+},3000);
 
-function addimages(){
-    list="";
-    dotList="";
-    i=0;
-    images.forEach((image)=>{
-        list+=`<div class="slide fade"><img class="images" src="${image.url}" alt="${image.alt}"> </div>`;
-        dotList+=`<span class="dot" onclick="currentSlide(${i})"></span>`;
-        i++
-    });
-    // list+=`<a class="prev" onclick="">`+ &#10094; + `</a>
-    //     <a class="next" onclick="">`+ &#10095; + `</a>`;
-    let slideshowContainer=document.getElementById("slideshowContainer");
-    slideshowContainer.innerHTML=list;
-    let more=document.getElementById("more");
-    more.innerHTML=dotList;
-
-    
-
+function pushSlides(n){
+    if(curIndex<0){curIndex=images.length-1;}
+    else{curIndex=(curIndex+n)%images.length;}
+    let slideshowContainer = document.getElementById("slideshowContainer");
+    slideshowContainer.style.transform=`translateX(-${carouselContainer.clientWidth*curIndex}px)`
 }
-
-
